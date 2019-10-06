92282a8d796af62de736ab7844467b8754214b08
Raj Sodhi <rajsodhiesq@gmail.com>
2019-10-06 16:14:38 -0700

    basic html pages set up

---
+++ app/data/data.js
@@ -0,0 +1,52 @@
+var bars = [
+    {
+        "name": "",
+        "photo": ""
+        "scores:" []
+    },
+    {
+        "name": "",
+        "photo": ""
+        "scores:" []
+    },
+    {
+        "name": "",
+        "photo": ""
+        "scores:" []
+    },
+    {
+        "name": "",
+        "photo": ""
+        "scores:" []
+    },
+    {
+        "name": "",
+        "photo": ""
+        "scores:" []
+    },
+    {
+        "name": "",
+        "photo": ""
+        "scores:" []
+    },
+    {
+        "name": "",
+        "photo": ""
+        "scores:" []
+    },
+    {
+        "name": "",
+        "photo": ""
+        "scores:" []
+    },
+    {
+        "name": "",
+        "photo": ""
+        "scores:" []
+    },
+    {
+        "name": "",
+        "photo": ""
+        "scores:" []
+    }
+]
--- server.js
+++ server.js
@@ -7,10 +7,10 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.erlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}));
 
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
+// require('./app/routing/apiRoutes.js')(app);
+// require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);
