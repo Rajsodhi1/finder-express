be4cd327f67df586caa22d470b8185a05502a3e4
Raj Sodhi <rajsodhiesq@gmail.com>
2019-10-07 17:43:39 -0700

    questions added

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+// var path = require("path");
 
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: false}));
 
 // require('./app/routing/apiRoutes.js')(app);
-// require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);
